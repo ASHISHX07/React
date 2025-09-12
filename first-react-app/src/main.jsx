f293f83ac559f89fa701be05e488b465d25237dc
Ashish <ashishsindhav02@outlook.com>
2025-09-12 20:35:09 +0530

    Some more changes

--- first-react-app/src/main.jsx
+++ first-react-app/src/main.jsx
@@ -1,19 +1,17 @@
-import { StrictMode, useState } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-function Counter() {
-  // Destructuring the array returned by useState
-  const [count, setCount] = useState(0);
-  
-  return (
-    <button onClick={() => setCount(count + 1)}>
-      Count: {count}
-    </button>
-  );
+function Fruit() {
+    let a = 4;
+    return (<>
+    <h1>{a < 6 ? "Apple" : "Banana"}</h1>
+    </>);
 }
 
-createRoot(document.getElementById('root')).render(
-  <Counter />
+createRoot(document.getElementById("root")).render(
+    <>
+        <Fruit />
+    </>
 );
